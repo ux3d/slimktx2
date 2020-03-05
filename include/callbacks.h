424b50fcd7428835959618b0f597f7a4044faf04
Fabian Wahlster <wahlster@ux3d.io>
2020-03-05 14:30:07 +0100

    move callback definition into own header

---
+++ include/callbacks.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2020 UX3D GmbH. All rights reserved.
+
+#pragma once
+
+#include <stdarg.h>
+#include <stddef.h>
+
+namespace ux3d
+{
+	namespace slimktx2
+	{
+		using AllocationFunc = void* (*)(void* _pUserData, size_t _size);
+		using FreeFunc = void(*)(void* _pUserData, void* _pData);
+
+		// IO - stream or file
+		using IOHandle = void*;
+
+		using ReadFunc = size_t(*)(void* _pUserData, IOHandle _file, void* _pData, size_t _size);
+		using WriteFunc = void(*)(void* _pUserData, IOHandle _file, const void* _pData, size_t _size);
+		using TellFunc = size_t(*)(void* _pUserData, const IOHandle _file);
+		using SeekFunc = bool(*)(void* _pUserData, IOHandle _file, size_t _offset);
+
+		using LogFunc = void(*)(void* _pUserData, const char* _pFormat, va_list args);
+
+		struct Callbacks
+		{
+			void* userData = nullptr; // holds allocator, user implementations
+
+			AllocationFunc allocate = nullptr;
+			FreeFunc free = nullptr;
+
+			ReadFunc read = nullptr;
+			WriteFunc write = nullptr;
+
+			TellFunc tell = nullptr;
+			SeekFunc seek = nullptr;
+
+			// optional
+			LogFunc log = nullptr;
+		};
+	}// !slimktx2
+} // ux3d
--- include/slimktx2.h
+++ include/slimktx2.h
@@ -1,48 +1,16 @@
-// Copyright (c) 2019 UX3D GmbH. All rights reserved.
+// Copyright (c) 2020 UX3D GmbH. All rights reserved.
 
 #pragma once
 
-#include <stddef.h>
-#include <stdint.h>
-#include <stdarg.h>
-#include <new.h>
+#include <new.h> // placement new
 
 #include "dfd.h"
+#include "callbacks.h"
 
 namespace ux3d
 {
 	namespace slimktx2
 	{
-		using AllocationFunc = void*(*)(void* _pUserData, size_t _size);
-		using FreeFunc = void(*)(void* _pUserData, void* _pData);
-
-		// IO - stream or file
-		using IOHandle = void*;
-
-		using ReadFunc = size_t(*)(void* _pUserData, IOHandle _file, void* _pData, size_t _size);
-		using WriteFunc = void(*)(void* _pUserData, IOHandle _file, const void* _pData, size_t _size);
-		using TellFunc = size_t(*)(void* _pUserData, const IOHandle _file);
-		using SeekFunc = bool(*)(void* _pUserData, IOHandle _file, size_t _offset);
-
-		using LogFunc = void(*)(void* _pUserData, const char* _pFormat, va_list args);
-
-		struct Callbacks
-		{
-			void* userData = nullptr; // holds allocator, file/stream handle
-
-			AllocationFunc allocate = nullptr;
-			FreeFunc free = nullptr;
-
-			ReadFunc read = nullptr;
-			WriteFunc write = nullptr;
-
-			TellFunc tell = nullptr;
-			SeekFunc seek = nullptr;
-
-			// optional
-			LogFunc log = nullptr;
-		};
-
 		enum class Format : uint32_t
 		{
 			UNDEFINED = 0u,
