7a4122b89c176901b624b15f8071bef14727e407
Fabian Wahlster <fwahlster@outlook.com>
2020-03-10 09:53:54 +0100

    add default malloc/free allocation callback

---
+++ source/DefaultAllocationCallback.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2020 UX3D GmbH. All rights reserved.
+
+#include "DefaultAllocationCallback.h"
+
+#include <stdlib.h>
+
+using namespace ux3d::slimktx2;
+
+inline Callbacks DefaultAllocationCallback::getCallback() const
+{
+	slimktx2::Callbacks callback{};
+
+	// userdata not required
+	callback.allocate = allocate;
+	callback.deallocate = deallocate;
+
+	return callback;
+}
+
+DefaultAllocationCallback::operator Callbacks() const
+{
+	return getCallback();
+}
+
+void* DefaultAllocationCallback::allocate(void* _pUserData, size_t _size)
+{
+	return malloc(_size);
+}
+
+void DefaultAllocationCallback::deallocate(void* _pUserData, void* _pData)
+{
+	free(_pData);
+}
